b92ca7fb61b4e3c5575b5a0b677b04a580436f7c
yijuzhou <yijuzhou@student.unimelb.edu.au>
2022-08-11 15:48:13 +1000

    change a bit

---
+++ Assets/ChangeShapeTo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShapeTo : MonoBehaviour {
+    [SerializeField]
+    private Mesh changeMeshTo;
+
+    private void OnCollisionEnter() {
+        var meshFilter = GetComponent<MeshFilter>();
+
+        meshFilter.mesh = changeMeshTo;
+
+    }
+}
